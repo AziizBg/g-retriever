9ddd3bd2df71edf4a702e06f9107ca44d5e1b3a8
AzizBenGhorbel <mohamedaziz.benghorbel@insat.ucar.tn>
2025-05-16 10:46:36 +0100

    fix

--- src/model/graph_llm.py
+++ src/model/graph_llm.py
@@ -187,3 +187,73 @@ class GraphLLM(torch.nn.Module):
             )
 
         return outputs.loss
+
+
+    def inference(self, samples):
+
+        # encode description and questions
+        questions = self.tokenizer(samples["question"], add_special_tokens=False)
+        descriptions = self.tokenizer(samples["desc"], add_special_tokens=False)
+
+        # encode special tokens
+        eos_user_tokens = self.tokenizer(EOS_USER, add_special_tokens=False)
+        bos_embeds = self.word_embedding(self.tokenizer(BOS, add_special_tokens=False, return_tensors='pt').input_ids[0])
+        pad_embeds = self.word_embedding(
+            torch.tensor(self.tokenizer.pad_token_id).to(self.word_embedding.weight.device)
+        ).unsqueeze(0)
+
+        # encode graphs
+        graph_embeds = self.encode_graphs(samples)
+        graph_embeds = self.projector(graph_embeds)
+
+        batch_size = len(samples['id'])
+        batch_inputs_embeds = []
+        batch_attention_mask = []
+        for i in range(batch_size):
+            # STRICTLY TRUNCATE INPUTS
+            max_allowed_desc = self.max_txt_len - len(questions.input_ids[i]) - len(eos_user_tokens.input_ids) - len(label_input_ids)
+            desc_ids = descriptions.input_ids[i][:max_allowed_desc]
+            
+            input_ids = desc_ids + questions.input_ids[i] + eos_user_tokens.input_ids + label_input_ids
+            inputs_embeds = self.word_embedding(torch.tensor(input_ids).to(self.model.device))
+            batch_inputs_embeds.append(inputs_embeds)
+            batch_attention_mask.append([1] * inputs_embeds.shape[0])
+
+        # pad inputs_embeds
+        max_length = max([x.shape[0] for x in batch_inputs_embeds])
+        for i in range(batch_size):
+            pad_length = max_length-batch_inputs_embeds[i].shape[0]
+            batch_inputs_embeds[i] = torch.cat([pad_embeds.repeat(pad_length, 1), batch_inputs_embeds[i]])
+            batch_attention_mask[i] = [0]*pad_length+batch_attention_mask[i]
+
+        inputs_embeds = torch.stack(batch_inputs_embeds, dim=0).to(self.model.device)
+        attention_mask = torch.tensor(batch_attention_mask).to(self.model.device)
+
+        with self.maybe_autocast():
+            outputs = self.model.generate(
+                inputs_embeds=inputs_embeds,
+                max_new_tokens=self.max_new_tokens,
+                attention_mask=attention_mask,
+                # do_sample=True,
+                use_cache=True  # IMPORTANT!
+            )
+        pred = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+        return {'id': samples['id'],
+                'pred': pred,
+                'label': samples['label'],
+                'question': samples['question'],
+                'desc': samples['desc'], }
+
+    def print_trainable_params(self):
+        trainable_params = 0
+        all_param = 0
+
+        for _, param in self.named_parameters():
+            num_params = param.numel()
+
+            all_param += num_params
+            if param.requires_grad:
+                trainable_params += num_params
+
+        return trainable_params, all_param
